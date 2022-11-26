f65a9203445d05ecc22b0d2801b122eecaae38e3
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-11-25 23:48:38 -0800

    Buttons work

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-const teamOneOne = document.getElementById()
+const teamOne = document.getElementById('team-one');
+const teamTwo = document.getElementById('team-two');
+
+// buttons 
+const teamBtnOne = document.getElementById('team-one-1');
+
+
+let countOne = 0;
+let countTwo = 0;
+
+teamBtnOne.addEventListener('click', onePoint)
+
+
+// function for each button
+function onePoint() {
+    countOne += 1;
+    teamOne.textContent = countOne;
+}
+
+function twoPoint() {
+    countOne += 1;
+    teamOne.textContent = countOne;
+}
+
+function threePoint() {
+    countOne += 1;
+    teamOne.textContent = countOne;
+}
+
+function twoPoint() {
+    countOne += 2;
+    countTwo += 2;
+
+    teamOne.textContent = countOne;
+    teamTwo.textContent = countTwo;
+}
+
+function threePoint() {
+    countOne += 3;
+    countTwo += 3;
+
+    teamOne.textContent = countOne;
+    teamTwo.textContent = countTwo;
+}
